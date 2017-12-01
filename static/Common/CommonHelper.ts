d1a926f902a4c2859efd12627c695670eba8b736
Lq <aa875191946@qq.com>
2017-12-01 16:37:13 +0800

    up

--- static/Common/CommonHelper.ts
+++ static/Common/CommonHelper.ts
@@ -8,5 +8,32 @@ class CommonHelper {
   {
 
   }
+  //数据列表 去重
+  public distinct(sourList:any[], target:string):any
+  {
+    console.log('distinct');
+    let ret_list = [];
+    if (sourList.length > 0)
+    {
+      for (let i = 0 ; i < sourList.length;i++)
+      {
+        if (sourList[i].hasOwnProperty(target))
+        {
+          let index = ret_list.findIndex(function (obj){
+            return sourList[i][target] == obj[target];
+          });
+          if (index == -1)
+          {
+            ret_list.push(sourList[i]);
+          }
+        }
+      }
+      return ret_list;
+    }
+    else
+    {
+      return sourList;
+    }
+  }
 }
 export {CommonHelper};
