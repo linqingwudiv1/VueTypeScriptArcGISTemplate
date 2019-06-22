964aaa0076ef5de5d21e17999c1a7760a598b099
LinQing <aa875191946@qq.com>
2019-06-22 18:07:18 +0800

    up

--- src/components/DevNav/component/PanolensExam/index.ts
+++ src/components/DevNav/component/PanolensExam/index.ts
@@ -1,47 +1,151 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import { Image } from 'element-ui';
 
 declare const PANOLENS:any;
 declare const THREE:any;
+
+
+
+class DTOModel_SceneItem
+{
+	constructor(_imagesrc:string, _position:any)
+	{
+		this.imagesrc = _imagesrc;
+		this.position = _position;
+	}
+	public imagesrc:string = '';
+	public position:any = new THREE.Vector3( 5000.00, 0.00, 0.00 );
+
+	public children:Array<DTOModel_SceneItem> = []; 
+}
+
+class PanoramaWrapper
+{
+	constructor(id:string)
+	{
+		let canvasElement = document.getElementById(id);
+		this.viewer = new PANOLENS.Viewer(
+			{
+				container:canvasElement ,
+				//output:'overlay',
+				autoHideInfospot :false,
+				controlBar: false,
+				horizontalView: true
+			});
+	}
+	public viewer:any;
+	public sceneData:Array<DTOModel_SceneItem> = [];  
+
+}
+
 @Component({
 })
 export default class PanolensExamComponent extends Vue {
+	public progressElement:HTMLElement;
 
-	private canvas:any;
+	public arr_panorama:Array<any> = [];
+	public dialogVisible:boolean = false;
+
+	private panoramaWrapper:PanoramaWrapper = null;
 	mounted() 
 	{
 
-		var infospot, infospot2, panorama:any, viewer:any;
+		console.log('mounted');
+		console.log(this.panoramaWrapper);
+		this.init();
 
-		infospot = new PANOLENS.Infospot(500,'../../../../static/img/s.png',false);
-		console.log(infospot);
-		infospot.addEventListener('click',function ()
-		{
-			panorama.load('../../../../static/img/panorama/2.jpg');
-		});
+
+		//setInterval(function (){
+		//	console.log(viewer.outputPositionEx());
+		//},1000);
+		//infospot.onClick = function (){console.log('testr');};
+	  }
+
+	  public loadScene():void
+	  {
+		  this.panoramaWrapper.sceneData.push(new DTOModel_SceneItem
+			( 
+				'../../../../static/img/panorama/firefighting/scene_1.jpg', 
+				new THREE.Vector3(-3478.26, -100, 3051.20))
+			);
+		  this.panoramaWrapper.sceneData.push(new DTOModel_SceneItem
+			(
+				'../../../../static/img/panorama/firefighting/scene_2.jpg', 
+				new THREE.Vector3(3590.22, -100, -3431.81)
+			));
+		  this.panoramaWrapper.sceneData.push(new DTOModel_SceneItem
+			(
+				'../../../../static/img/panorama/firefighting/scene_3.jpg', 
+				new THREE.Vector3(2799.18, -100, 4060.24)
+			));
+	  }
+
+	  public init():boolean
+	  {
+		  this.progressElement 	= document.getElementById( 'progress' );
+		  //this.canvasElement 	= document.getElementById('panolensCanvas');
+
+		  this.initPanorama();
+		  return true;
+	  }
+
+	  public initPanorama():boolean
+	  {   
+		this.panoramaWrapper = new PanoramaWrapper ('panolensCanvas');
+		this.loadScene();
 		
-		//infospot.position.set( 5000.00, -665.23, -3996.49 );
+		let infospot = new PANOLENS.Infospot(500, '../../../../static/img/s.png', true);
+
+		infospot.addEventListener('click', function ()
+		{
+			//panorama.load('../../../../static/img/panorama/2.jpg');
+		});
 
 		infospot.position.set( 5000.00, -665.23, -996.49 );
 
-		//infospot.rotation.set(50,50,50);
+		this.panoramaWrapper.viewer ;
 
-		panorama = new PANOLENS.ImagePanorama( '../../../../static/img/panorama/1.jpg' );
+
+		for (let i = 0 ; i < this.panoramaWrapper.sceneData.length; i++)
+		{
+			let temp_panorama = new PANOLENS.ImagePanorama( this.panoramaWrapper.sceneData[i].imagesrc );
+
+			temp_panorama.addEventListener( 'progress', this.onProgress );
+			temp_panorama.addEventListener( 'enter', this.onEnter );
 		
-		panorama.add( infospot );
-		//panorama.add( infospot2 );
+			this.arr_panorama.push( temp_panorama );
+		}
 
-		this.canvas = document.getElementById('panolensCanvas');
-		viewer = new PANOLENS.Viewer(
-			{
-				container:this.canvas,
-				output:'overlay',
-				autoHideInfospot :false
-			});
+		for (let i = 0; i < this.panoramaWrapper.sceneData.length; i++)
+		{
+			let cur_panorama = this.arr_panorama[i];
+			let next_panorma =  this.arr_panorama[(i + 1) < this.arr_panorama.length ? (i + 1) : 0 ];
+			
+			let vec = this.panoramaWrapper.sceneData[i].position;
+			cur_panorama.link(next_panorma, vec, 650);
+
+			
+			console.log(this.arr_panorama[0].linkedSpots [0]);
+
+			let el = document.createElement('h2');
+			el.innerText = '疏散点';
+			//cur_panorama.linkedSpots[0].addHoverElement(el);
+			/*
+			this.arr_panorama[0].linkedSpots[0] .addHoverElement(el);
+			console.log(this.arr_panorama[0].linkedSpots[0].element);
+			this.arr_panorama[0].linkedSpots[0].setText('abc');
+			let temp1 = this.arr_panorama[0].linkedSpots[0];
+			temp1.lockHoverElement();
+			temp1.element.style.display = 'block';
+			temp1.lockHoverElement();
+			*/
+			this.panoramaWrapper.viewer.add(cur_panorama);
 
 
-		viewer.add( panorama );
-		viewer.outputPositionEx = function ()
+		}
+
+		this.panoramaWrapper.viewer.outputPositionEx = function ()
 		{
 			const intersects = this.raycaster.intersectObject( this.panorama, true );
 
@@ -51,23 +155,46 @@ export default class PanolensExamComponent extends Vue {
 				const world = this.panorama.getWorldPosition( new THREE.Vector3() );
 
 				point.sub( world ).multiply( converter );
+
 				return point;	
 			}
 			return null;
 		};
 
-		console.log(viewer);
 
-		setInterval(function (){
-			console.log(viewer.outputPositionEx());
-		},1000);
-		infospot.onClick = function (){console.log('testr');};
+
+
+		return true;
 	  }
-	  
 
-	  public loadScene():void 
+	  public onProgress (event:any):void
+	  {
+		console.log('onProgress');
+        let progress = event.progress.loaded / event.progress.total * 100;
+        this.progressElement.style.width = progress + '%';
+		if ( progress === 100 ) 
+		{
+          this.progressElement.classList.add( 'finish' );
+		}
+	  }
+
+	  public onEnter(event:any):void 
+	  {
+		console.log('onEnter');
+		this.progressElement.style.width = '0';
+        this.progressElement.classList.remove( 'finish' );
+	  }
+
+	  public gotofocus():void 
 	  {
 
+		  this.panoramaWrapper.viewer.panorama.linkedSpots[0].focus();
+	  }
+
+	  public openDialog():void 
+	  {
+
+		this.dialogVisible = true;
 	  }
 }
  
--- src/components/DevNav/component/PanolensExam/index.vue
+++ src/components/DevNav/component/PanolensExam/index.vue
@@ -1,58 +1,90 @@
 <template>
-    <div id="PanolensDiv">
-          <!-- <div class="credit"><a href="https://github.com/pchen66/panolens.js">Panolens.js</a> infospot example. Images from Google Streetview API</div> -->
-    
-    <div id="desc-container" style="display:none">
-      <iframe src="https://www.youtube.com/embed/D7icsuamx5E"></iframe>
-      <div class="title">China's Forgotten War</div>
-      <div class="text">WWII came to the small town of Tai’erzhuang in central China – and it was never the same again. The town was strategically placed, on the north-south transport railway corridor and the ancient Grand Canal, and so was a focus of the Japanese Imperial army as it advanced. Li Jing Shan was only a child when his family fled the fighting. They returned to find their home, and most of the town, in ruins.</div>
+<div id="PanolensDiv">
+
+    <div class="credit">疏散通道720° VR全景展示Demo</div>
+    <div id="progress"></div>
+
+    <div class="credit-footer">
+        <el-button type="text" v-on:click="gotofocus">定位疏散点</el-button>
     </div>
+
+        <div class="credit-footer" style="bottom : 55px;">
+        <el-button type="text" v-on:click="openDialog">疏散平面图</el-button>
     </div>
+
+  <el-dialog title="平面图"  :visible.sync="dialogVisible"  width="95%" custom-class="panelDialog">
+          <el-card :body-style="{ padding: '0px' }">
+                <img src="../../../../../static/img/q.jpg" class="image" style="width:100%">
+                <div style="padding: 14px;">
+
+                </div>
+              </el-card>
+  </el-dialog>
+
+</div>
 </template>
 
 <script lang="ts" src="./index.ts">
 </script>
-    <style>
+    
+<style>
+#progress {
+    width: 0;
+    height: 5px;
+    position: fixed;
+    top: 0;
+    background: #5c91b4;
+    -webkit-transition: opacity 0.5s ease;
+    transition: opacity 0.5s ease;
+}
+
+#progress.finish {
+    opacity: 0;
+}
 
 
-      a:link, a:visited{
-        color: #bdc3c7;
-      }
+.panelDialog
+{
+  background: rgba(31,31,31,0.9);
 
-      .credit{
-        position: absolute;
-        text-align: center;
-        width: 100%;
-        padding: 20px 0;
-        color: #fff;
-      }
+}
 
-      #desc-container {
-        max-width: 500px;
-        max-height: 500px;
-        min-width: 200px;
-        min-height: 250px;
-        background: #fff;
-        color: #000;
-        border-radius: 3px;
-        overflow: auto;
-        -webkit-overflow-scrolling: touch;
-      }
+.panelDialog .el-dialog__title
+{
+  color: white;
+}
+.credit {
+    position: absolute;
+    text-align: center;
+    width: 100%;
+    padding: 20px 0;
+    background: rgba(40,40,40, 0.3);;
+    color: white;
 
-      #desc-container > iframe {
-        border: none;
-        width:100%;
-      }
+}
 
-      .title {
-        font-size: 1.5em;
-        text-align: center;
-        padding: 5px;
-      }
+.credit-footer {
+    position: absolute;
+    text-align: center;
+    width: 6rem;
+    padding: 5px 0;
+    background: rgba(40,40,40, 0.9);
+    color: white;
+    bottom: 0;
+    margin: 1rem;
+    right: 0;
+    box-shadow: rgba(48, 49, 51, 0.58) 2px 2px 2px 0px;
+}
 
-      .text {
-        padding: 0 20px 20px 20px;
-      }
-    </style>
+.title {
+    font-size: 1.5em;
+    text-align: center;
+    padding: 5px;
+}
+
+.text {
+    padding: 0 20px 20px 20px;
+}
+</style>
 <style lang="stylus">
 </style>
